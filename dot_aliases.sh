d74df05a745a5c4f7f1b0aa14986045677eb3ec5
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-02-16 22:29:46 -0700

    Initial commit

---
+++ dot_aliases.sh
@@ -0,0 +1,58 @@
+## NPM
+alias n='npm'
+alias ni='n i'
+alias nr='n run'
+
+## PNPM
+alias p='pnpm'
+alias pi='p i'
+alias pch='p changeset'
+alias pe='p exec'
+alias px='p dlx'
+alias pc='p pc'
+alias dev='p dev'
+function pf {
+    FILTER=$1
+    shift
+    p --filter "*"$FILTER"*" $@
+}
+
+alias g='git'
+alias gm='g merge'
+alias gp='g pull'
+alias gc='g checkout'
+alias ga='g add'
+alias gac='ga . && git commit'
+alias gcm='g commit -m'
+alias gacm='ga . && gcm'
+alias grh='g reset --hard'
+
+function gb {
+    git for-each-ref \
+        --sort=-committerdate \
+        refs/heads/ \
+        --format='%(HEAD) %(align:45)%(color:yellow)%(refname:short)%(color:reset)%(end) %(color:red)%(objectname:short)%(color:reset) %(contents:subject)'
+}
+
+alias c='code'
+alias cr='code -r'
+function zc {
+    z $1 && c .
+}
+function zcr {
+    z $1 && cr .
+}
+
+function gacmp {
+    ga . && gcm $1 && g push
+}
+
+function gcmp {
+    gcm $1 && g push
+}
+
+alias pchgacmp='pch && gacmp'
+
+alias ls='exa -a'
+alias ll='exa -lag --header'
+alias mkdir='mkdir -p'
