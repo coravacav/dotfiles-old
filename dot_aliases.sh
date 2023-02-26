9074cc21d5202ac0f542c48f72aed438228ec0e8
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-02-25 18:59:47 -0700

    settings

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -34,6 +34,8 @@ function gb {
         --format='%(HEAD) %(align:45)%(color:yellow)%(refname:short)%(color:reset)%(end) %(color:red)%(objectname:short)%(color:reset) %(contents:subject)'
 }
 
+alias prune_git_branches_merged_to_main='git checkout -q main && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base main $branch) && [[ $(git cherry main $(git commit-tree $(git rev-parse "$branch^{tree}") -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
+
 alias c='code'
 alias cr='code -r'
 function zc {
