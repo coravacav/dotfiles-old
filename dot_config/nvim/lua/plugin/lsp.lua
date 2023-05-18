eb0c6a1b8316bd1e105bd5c397bb9aa2c0c36a95
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-18 17:55:55 -0600

    add more config stuff

--- dot_config/nvim/lua/plugin/lsp.lua
+++ dot_config/nvim/lua/plugin/lsp.lua
@@ -1,7 +1,67 @@
 return {
-    "lmburns/lf.nvim",
-     dependencies = {
-        "akinsho/toggleterm.nvim",
-        "nvim-lua/plenary.nvim",
-     }
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+        pcall(vim.cmd, 'MasonUpdate')
+
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'saadparwaiz1/cmp_luasnip'},
+    {'rafamadriz/friendly-snippets'},
+  },
+  config = function()
+      local lsp = require('lsp-zero').preset({})
+
+      lsp.on_attach(function(client, bufnr)
+          lsp.default_keymaps({buffer = bufnr})
+      end)
+
+      -- (Optional) Configure lua language server for neovim
+      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+      lsp.setup()
+
+
+      -- You need to setup `cmp` after lsp-zero
+      local cmp = require('cmp')
+
+      local cmp_action = require('lsp-zero').cmp_action()
+
+      cmp.setup({
+          mapping = {
+
+              -- `Enter` key to confirm completion
+              ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+              -- Ctrl+Space to trigger completion menu
+              ['<C-Space>'] = cmp.mapping.complete(),
+
+
+              -- Navigate between snippet placeholder
+              ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+              ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          },
+          sources = {
+              {name = 'path'},
+              {name = 'nvim_lsp'},
+              {name = 'buffer', keyword_length = 3},
+              {name = 'luasnip', keyword_length = 2},
+          },
+      })
+  end
 }
---
+++ dot_config/nvim/lua/plugin/rosepine.lua
@@ -0,0 +1,10 @@
+return {
+    'rose-pine/neovim',
+    config = function ()
+        require('rose-pine').setup({
+            variant = 'moon',
+        })
+        vim.cmd('colorscheme rose-pine')
+    end
+
+}
