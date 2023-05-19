b129ffd041f3e8e49d107b0bd8a4aa74a278e500
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-19 00:10:21 -0600

    Update .config/nvim/lua/plugin/fugitive.lua

--- dot_config/nvim/lua/plugin/fugitive.lua
+++ dot_config/nvim/lua/plugin/fugitive.lua
@@ -1,7 +1,7 @@
 -- This is the git plugin
 return {
     'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-    end
+    keys = {
+        {'<leader>gs', function() vim.cmd.Git() end }
+    },
 }
