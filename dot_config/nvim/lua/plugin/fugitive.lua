5d1ce7002a00901f20ca6b722118351c52c5d1c7
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-19 02:31:25 -0600

    Update .config/nvim/lua/plugin/fugitive.lua Update .config/nvim/lua/plugin/telescope.lua

--- dot_config/nvim/lua/plugin/fugitive.lua
+++ dot_config/nvim/lua/plugin/fugitive.lua
@@ -2,6 +2,7 @@
 return {
     'tpope/vim-fugitive',
     keys = {
-        {'<leader>gs', function() vim.cmd('vert Git | vert res 40') end }
+        {'<leader>gss', ':vert Git | vert res 40<cr>', desc = 'Show fugitive (git) small, on the side'},
+        {'<leader>gsf', ':Git<cr>', desc = 'Show fugitive (git) with (bad) default behavior'}
     },
 }
--- dot_config/nvim/lua/plugin/telescope.lua
+++ dot_config/nvim/lua/plugin/telescope.lua
@@ -46,31 +46,53 @@ return {
             extensions = {
                 ["zf-native"] = {
                     file = {
+                        -- I installed this for a reason
                         enable = true,
+
+                        -- Ooh, shiny 
                         highlight_results = true,
+
+                        -- Crazy enough, while searching for files, considering the filename is useful.
                         match_filename = true,
                     },
                     generic = {
+                        -- It's good in general, or at least not worse.
                         enable = true,
+
+                        -- Ooh, shiny part 2
                         highlight_results = true,
+
+                        -- Don't particularly care if I'm not searching for a file
                         match_filename = false,
                     }
                 },
                 file_browser = {
+                    -- Nothing shall hide.
                     hidden = true,
+
+                    -- I don't want to respect the .gitignore, browing files means see all
                     respect_gitignore = false,
+
+                    -- honestly I don't think this works but I'd want it anyway
                     hijack_netrw = true,
                 }
             },
             defaults = {
+                -- Seeing the preview on top is nice, since I don't really need to see all the results
                 layout_strategy = 'vertical',
                 layout_config = {
+                    -- I have a screen, might as well use most of it
                     vertical = { width = 0.9, height = 0.9 },
+
+                    -- show a preview no matter what
                     preview_cutoff = 0
                 },
                 mappings = {
                     i = {
+                        -- because this isn't bound by default for some reason
                         ["<esc>"] = actions.close,
+
+                        -- I want to be able to create a file like I normally would
                         ["<c-n>"] = file_browser.create,
                     }
                 }
@@ -80,6 +102,7 @@ return {
         -- This improves ordering of results when searching for files prioritizes filenames
         telescope.load_extension("zf-native")
 
+        -- A mildly better extension (at least in my eyes) for file browsing.
         telescope.load_extension("file_browser")
     end,
 }
