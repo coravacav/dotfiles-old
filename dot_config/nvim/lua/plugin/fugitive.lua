94c36ffdf1f54510046499f7940a167abfe87b1c
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-18 20:42:39 -0600

    Update .config/nvim/lua/plugin/fugitive.lua Update .config/nvim/lua/plugin/lualine.lua Update .config/nvim/lua/plugin/treesitter.lua

--- dot_config/nvim/lua/plugin/fugitive.lua
+++ dot_config/nvim/lua/plugin/fugitive.lua
@@ -1,5 +1,7 @@
 -- This is the git plugin
 return {
-	'tpope/vim-fugitive'	
+    'tpope/vim-fugitive',
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+    end
 }
-
--- dot_config/nvim/lua/plugin/lualine.lua
+++ dot_config/nvim/lua/plugin/lualine.lua
@@ -1,6 +1,10 @@
 return {
     'nvim-lualine/lualine.nvim',
     config = function()
-        require('lualine').setup()
+        require('lualine').setup({
+            sections = {
+                lualine_x = { 'selectioncount', 'filetype' }
+            }
+        })
     end,
 }
