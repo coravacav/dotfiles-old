0e293fdfa7f7bfa10c31c5cb3d312ef233cd4f71
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-18 18:50:40 -0600

    finish lsp

--- dot_config/nvim/lua/plugin/lsp.lua
+++ dot_config/nvim/lua/plugin/lsp.lua
@@ -1,67 +1,68 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  dependencies = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {                                      -- Optional
-      'williamboman/mason.nvim',
-      build = function()
-        pcall(vim.cmd, 'MasonUpdate')
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {
+            -- Optional
+            'williamboman/mason.nvim',
+            build = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      end,
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },     -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' },     -- Required
+
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'saadparwaiz1/cmp_luasnip' },
     },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    config = function()
+        local lsp = require('lsp-zero').preset({})
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-
-    {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'rafamadriz/friendly-snippets'},
-  },
-  config = function()
-      local lsp = require('lsp-zero').preset({})
-
-      lsp.on_attach(function(client, bufnr)
-          lsp.default_keymaps({buffer = bufnr})
-      end)
-
-      -- (Optional) Configure lua language server for neovim
-      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-      lsp.setup()
+        lsp.on_attach(function(client, bufnr)
+            lsp.default_keymaps({ buffer = bufnr })
+            local opts = { buffer = bufnr }
 
 
-      -- You need to setup `cmp` after lsp-zero
-      local cmp = require('cmp')
+            vim.keymap.set({ 'n', 'x' }, '<leader>fa', function()
+                vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+            end, opts)
+        end)
 
-      local cmp_action = require('lsp-zero').cmp_action()
+        -- (Optional) Configure lua language server for neovim
+        require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-      cmp.setup({
-          mapping = {
+        lsp.setup()
 
-              -- `Enter` key to confirm completion
-              ['<CR>'] = cmp.mapping.confirm({select = false}),
+        -- You need to setup `cmp` after lsp-zero
+        local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
 
-              -- Ctrl+Space to trigger completion menu
-              ['<C-Space>'] = cmp.mapping.complete(),
+        cmp.setup({
+            mapping = {
+                -- `Enter` key to confirm completion
+                ['<cr>'] = cmp.mapping.confirm({ select = false }),
 
+                -- Ctrl+Space to trigger completion menu
+                ['<c-space>'] = cmp.mapping.complete(),
 
-              -- Navigate between snippet placeholder
-              ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-              ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-          },
-          sources = {
-              {name = 'path'},
-              {name = 'nvim_lsp'},
-              {name = 'buffer', keyword_length = 3},
-              {name = 'luasnip', keyword_length = 2},
-          },
-      })
-  end
+                -- Navigate between snippet placeholder
+                -- ['<c-f>'] = cmp_action.luasnip_jump_forward(),
+                -- ['<c-b>'] = cmp_action.luasnip_jump_backward(),
+            },
+            sources = {
+                { name = 'path' },
+                { name = 'nvim_lsp' },
+                { name = 'buffer',  keyword_length = 3 },
+                { name = 'luasnip', keyword_length = 2 },
+            },
+        })
+    end
 }
