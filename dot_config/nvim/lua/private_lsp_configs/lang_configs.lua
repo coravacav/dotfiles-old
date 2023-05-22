a1a288095de156209c19cd68d72f52d2be2f01dc
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-21 23:43:17 -0600

    Remove .config/nvim/lua/lsp_configs/cmp.lua Remove .config/nvim/lua/lsp_configs/common_keybinds.lua Remove .config/nvim/lua/lsp_configs/ensure_installed.lua Remove .config/nvim/lua/lsp_configs/lang_configs.lua Remove .config/nvim/lua/lsp_configs/on_attach.lua Remove .config/nvim/lua/lsp_configs/prettier.lua Remove .config/nvim/lua/lsp_configs/rust.lua Remove .config/nvim/lua/lsp_configs/typescript.lua

--- dot_config/nvim/lua/private_lsp_configs/lang_configs.lua
+++
@@ -1,5 +0,0 @@
--- This is where I individually add imports for all language specific lsp configs
-
-require('lsp_configs/rust')
-require('lsp_configs/typescript')
-require('lsp_configs/prettier')
--- dot_config/nvim/lua/private_lsp_configs/typescript.lua
+++
@@ -1,8 +0,0 @@
-require('typescript').setup({
-    debug = false,
-    server = {
-        on_attach = function(client, bufnr)
-            -- vim.keymap.set('n', '<leader>ci', '<cmd>TypescriptAddMissingImports<cr>', { buffer = bufnr })
-        end
-    }
-})
