15d847c887f1b456f805642d244d943f8233bd79
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-05-31 00:31:41 +0300

    Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'tokyonight_moon'
+config.font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'DemiBold' })
+config.font_size = 24.0
+
+return config
