dabdd15a5be4f4813b91adaf68a0848654dd2621
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-06-04 20:17:45 +0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugin/rosepine.lua

--- dot_config/nvim/lua/plugin/rosepine.lua
+++ dot_config/nvim/lua/plugin/rosepine.lua
@@ -1,16 +1,14 @@
 return {
     'rose-pine/neovim',
-    dependencies = { 'xiyaowong/transparent.nvim' },
     config = function()
         require('rose-pine').setup({
             variant = 'moon',
             dark_variant = 'moon',
+            disable_background = true,
+            disable_float_background = true,
         })
 
         vim.cmd('colorscheme rose-pine')
-
-        require('transparent').setup {}
-        vim.cmd(':TransparentEnable')
     end,
     lazy = false,
     priority = 1000,
