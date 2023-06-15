cfd9e3115e65ca57cf8789b144fbf9dde522528c
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-06-15 10:43:31 -0600

    Update .config/nvim/lua/plugin/onemap.lua

--- dot_config/nvim/lua/plugin/onemap.lua
+++ dot_config/nvim/lua/plugin/onemap.lua
@@ -1,6 +1,7 @@
 return {
     'coravacav/onemap.nvim',
     event = 'VeryLazy',
+    -- dir = '/Users/st/Documents/Programming/personal/onemap.nvim',
     opts = {
         groups = { 'lsp' },
         buffer_local_groups = { 'lsp' }
