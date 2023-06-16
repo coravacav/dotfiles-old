cb470d1ec0f83132007bf56b91f6c35091668c19
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-06-16 08:27:37 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugin/onemap.lua

--- dot_config/nvim/lua/plugin/onemap.lua
+++ dot_config/nvim/lua/plugin/onemap.lua
@@ -1,6 +1,7 @@
 return {
     'coravacav/onemap.nvim',
     event = 'VeryLazy',
+    -- dir = '/home/wrxl/onemap.nvim',
     -- dir = '/Users/st/Documents/Programming/personal/onemap.nvim',
     opts = {
         groups = { 'lsp' },
