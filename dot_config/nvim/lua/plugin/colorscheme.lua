044ee0d208e468db0d5aad601787db65ead5b963
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-06-18 10:58:07 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugin/colorscheme.lua

--- dot_config/nvim/lua/plugin/colorscheme.lua
+++ dot_config/nvim/lua/plugin/colorscheme.lua
@@ -4,14 +4,14 @@ return {
         require('rose-pine').setup({
             variant = 'moon',
             dark_variant = 'moon',
-            disable_background = true,
+            -- disable_background = true,
         })
 
         vim.cmd('colorscheme rose-pine')
 
-        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+        -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
     lazy = false,
     priority = 1000,
