d2be6c69bf5f020250f7b325fd4bcaeb7f4f290c
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-03 20:41:02 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugin/colorscheme.lua Update .config/wezterm/wezterm.lua

--- dot_config/nvim/lua/plugin/colorscheme.lua
+++ dot_config/nvim/lua/plugin/colorscheme.lua
@@ -7,7 +7,7 @@ return {
             -- disable_background = true,
         })
 
-        vim.cmd('colorscheme rose-pine')
+        vim.cmd.colorscheme "rose-pine"
 
         -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
         -- vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
