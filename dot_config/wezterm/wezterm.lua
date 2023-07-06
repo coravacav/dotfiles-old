9383246e2e7dac755239a96dd8cf46d0de5b5e1a
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-06 00:35:06 -0600

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ end
 
 config.color_scheme = 'tokyonight_moon'
 config.font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'DemiBold' })
-config.font_size = 24.0
+config.font_size = 20.0
 config.line_height = 0.9
 config.window_padding = {
     left = 8,
