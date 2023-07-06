eaf8c863d447bc0af54e5a475bc7a43730dd2d1f
Stefan Todorov <stefan.b.todorov@gmail.com>
2023-07-06 02:06:07 -0600

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ local config = {}
 -- elseif wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
 -- end
 
-config.window_background_image = 'bg.jpg'
+config.window_background_image = '~/.config/wezterm/bg.jpg'
 
 config.color_scheme = 'tokyonight_moon'
 config.font = wezterm.font('IosevkaTerm Nerd Font Mono', { weight = 'DemiBold' })
