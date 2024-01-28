16021b7cf56250d6a0a7e748a66aeb9b63af530b
Stefan Todorov <stefan.b.todorov@gmail.com>
2024-01-28 12:22:04 -0700

    Update .aliases.sh

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -76,4 +76,4 @@ alias cdp='cd $(git rev-parse --show-toplevel)'
 alias fcd='cd $(fd -t d | fzf)'
 
 alias bench_ts='pnpm exec tsc --generateTrace traceDir && pnpm dlx @typescript/analyze-trace traceDir'
-alias cc='cargo clippy'
+alias ccl='cargo clippy'
